a32d0d44ce203cddd6cc52b4f35730b5fcc6f723
Damien Meere <damien.meere@bt.com>
2020-05-06 16:05:02 +0000

    database created and linked to todo application, linked to admin panel and template page

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    # Blank in this instance just means that field will not be nullable when set to false and if we've said true then we would be allowed to have a blank field or a nullable field
+    name = models.CharField(max_length=30, blank=False)
+    done = models.BooleanField(blank=False, default=False)
+
+    #dunder method to return the name of the object
+    def __str__(self):
+        return self.name
